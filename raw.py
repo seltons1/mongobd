ab6d5180e6a4d1175c370e73ddf3eba3f3cac11d
Elton Santos <seltons1@gmail.com>
2023-05-11 11:36:57 -0300

    Insert itens

--- raw.py
+++ raw.py
@@ -6,10 +6,16 @@ def find(collection: str, key: str, value: str):
 
     collection_obj = connection.get_collection(collection)
 
-    #search_filter = {f'{key}' : f'{value}'}
-    search_filter = {"name" : "Dellaoc"}
+    search_filter = {f'{key}' : f'{value}'}
+    #search_filter = {"name" : "Dellaoc"}
     response = collection_obj.find(search_filter)
     for reg in response: print(reg)
 
+def insert(collection: str, values: dict):
+
+    collection_obj = connection.get_collection(collection)
+
+    collection_obj.insert_one(values)
 
 find("user","name","Dellaoc")
+insert("user",{"name":"Ryzen","size":[5,7,9]})
