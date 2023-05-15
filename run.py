9f43902b492624e27fb35c1169f3dee3a6ef72c8
Elton Santos <seltons1@gmail.com>
2023-05-15 20:43:06 -0300

    find methods

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ db_handle.connect_to_db()
 connection = db_handle.get_db_connection()
 
 user_repository = UserRepository(connection)
-user_repository.insert_document({"name":"AOC","val":[5,9,6]})
+user_repository.insert_document({"name":"AOC","pedidos":{"monitor": 2,"cpu": 10,"estabilizador":5}})
 
 list_of_documents= [
     {"teste":"1"},
@@ -14,7 +14,25 @@ list_of_documents= [
     {"teste 3":"3"},
 ]
 user_repository.insert_list_of_documents(list_of_documents)
+response = user_repository.select_many({"name": "Ryzen 2"}) # filter options
+print(response)
+print(" ")
+response2 = user_repository.select_one({"name": "Ryzen 2"})
+print(response2)
+print(" ")
+response3 = user_repository.select_if_property_exists()
+print(response3)
+print(" ")
+response4 = user_repository.select_many_order({"name": "AOC"}) # filter options
+print(response4)
 
+print(" ")
+response5 = user_repository.select_or() # filter options
+print(response5)
+
+print(" ")
+response6 = user_repository.select_by_object_id() # filter options
+print(response6)
 
 
 
